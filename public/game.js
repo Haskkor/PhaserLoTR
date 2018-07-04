ca30cca0cf7eff83612411df28789ad56d360d21
Jeremy Farnault <haskkor@gmail.com>
2018-07-05 09:09:14 +1200

    Adding turrets in the tower game

--- public/game.js
+++ public/game.js
@@ -11,63 +11,98 @@ const config = {
   }
 }
 
-const game = new Phaser.Game(config);
-let enemies;
+const game = new Phaser.Game(config)
+let enemies
+let path
 
-const ENEMY_SPEED = 1/10000;
+const ENEMY_SPEED = 1 / 10000
 
 const Enemy = new Phaser.Class({
   Extends: Phaser.GameObjects.Image,
-  initialize:
-  Enemy = (scene) => {
-    Phaser.GameObjects.Image.call(this, scene, 0, 0, 'sprites', 'enemy');
-    this.follower = { t: 0, vec: new Phaser.Math.Vector2() };
+
+  initialize: function Enemy (scene) {
+    Phaser.GameObjects.Image.call(this, scene, 0, 0, 'sprites', 'enemy')
+    this.follower = {t: 0, vec: new Phaser.Math.Vector2()}
   },
-  startOnPath: () => {
-    this.follower.t = 0;
-    path.getPoint(this.follower.t, this.follower.vec);
-    this.setPosition(this.follower.vec.x, this.follower.vec.y);
+  startOnPath: function () {
+    this.follower.t = 0
+    path.getPoint(this.follower.t, this.follower.vec)
+    this.setPosition(this.follower.vec.x, this.follower.vec.y)
   },
-  update: (time, delta) => {
-    this.follower.t += ENEMY_SPEED * delta;
-    path.getPoint(this.follower.t, this.follower.vec);
-    this.setPosition(this.follower.vec.x, this.follower.vec.y);
+  update: function (time, delta) {
+    this.follower.t += ENEMY_SPEED * delta
+    path.getPoint(this.follower.t, this.follower.vec)
+    this.setPosition(this.follower.vec.x, this.follower.vec.y)
     if (this.follower.t >= 1) {
-      this.setActive(false);
-      this.setVisible(false);
+      this.setActive(false)
+      this.setVisible(false)
+    }
+  }
+})
+
+const Turret = new Phaser.Class({
+  Extends: Phaser.GameObjects.Image,
+
+  initialize: function Turret (scene) {
+    Phaser.GameObjects.Image.call(this, scene, 0, 0, 'sprites', 'turret')
+    this.nextTic = 0
+  },
+  place: function (i, j) {
+    this.y = i * 64 + 64 / 2
+    this.x = j * 64 + 64 / 2
+    map[i][j] = 1
+  },
+  update: function (time, delta) {
+    if (time > this.nextTic) {
+      this.nextTic = time + 1000
     }
   }
 })
 
 function preload () {
-  this.load.atlas('sprites', 'assets/spritesheet.png', 'assets/spritesheet.json');
-  this.load.image('bullet', 'assets/bullet.png');
+  this.load.atlas('sprites', 'assets/spritesheet.png', 'assets/spritesheet.json')
+  this.load.image('bullet', 'assets/bullet.png')
 }
 
 function create () {
-  const graphics = this.add.graphics();
+  const graphics = this.add.graphics()
+  drawGrid(graphics)
 
-  const path = this.add.path(96, -32);
-  path.lineTo(96, 164);
-  path.lineTo(480, 164);
-  path.lineTo(480, 544);
+  path = this.add.path(96, -32)
+  path.lineTo(96, 164)
+  path.lineTo(480, 164)
+  path.lineTo(480, 544)
 
-  graphics.lineStyle(3, 0xffffff, 1);
-  path.draw(graphics);
+  graphics.lineStyle(3, 0xffffff, 1)
+  path.draw(graphics)
 
-  enemies = this.add.group({ classType: Enemy, runChildUpdate: true });
-  this.nextEnemy = 0;
+  enemies = this.add.group({classType: Enemy, runChildUpdate: true})
+  turrets = this.add.group({classType: Turret, runChildUpdate: true})
+  this.nextEnemy = 0
 }
 
 function update (time, delta) {
   if (time > this.nextEnemy) {
-    const enemy = enemies.get();
+    const enemy = enemies.get()
     if (enemy) {
       console.log('in update enemy')
-      enemy.setActive(true);
-      enemy.setVisible(true);
-      enemy.startOnPath();
-      this.nextEnemy = time + 2000;
+      enemy.setActive(true)
+      enemy.setVisible(true)
+      enemy.startOnPath()
+      this.nextEnemy = time + 2000
     }
   }
+}
+
+function drawGrid (graphics) {
+  graphics.lineStyle(1, 0x0000ff, 0.8)
+  for (let i = 0; i < 8; i++) {
+    graphics.moveTo(0, i * 64)
+    graphics.lineTo(640, i * 64)
+  }
+  for (let j = 0; j < 10; j++) {
+    graphics.moveTo(j * 64, 0)
+    graphics.lineTo(j * 64, 512)
+  }
+  graphics.strokePath()
 }
